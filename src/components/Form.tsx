5c338ff2fc4a0c0a5f4b0a925d40334035df0bc1
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-08-19 20:28:28 +0200

    adding data entry form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,46 @@
+import { categories } from "../data/category"
 
-type Props = {}
-
-const Form = (props: Props) => {
+const Form = () => {
       return (
             <form className="space-y-5 bg-white shadow p-10 rounded-lg">
                   <div className="grid grid-cold-1 gap-3">
-                        <label htmlFor="category">Categoria:</label>
+                        <label htmlFor="category" className="font-bold">Categoria:</label>
                         <select
-                              className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                              className="border border-slate-300  p-2 rounded-lg w-full bg-white"
                               name=""
                               id="category"
-                        ></select>
+                        >
+                              {categories.map(category => (
+                                    <option key={category.id} value={category.id}>{category.name}</option>
+                              ))}
+                        </select>
                   </div>
+
+                  <div className="grid grid-cold-1 gap-3">
+                        <label htmlFor="activity" className="font-bold">Actividad:</label>
+                        <input
+                              className="border border-slate-300 p-2 rounded-lg"
+                              type="text"
+                              id="activity"
+                              placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Presas, Bicicleta"
+                        />
+                  </div>
+
+                  <div className="grid grid-cold-1 gap-3">
+                        <label htmlFor="category" className="font-bold">Categorias:</label>
+                        <input
+                              className="border border-slate-300 p-2 rounded-lg"
+                              type="number"
+                              id="category"
+                              placeholder="Calorias Ej. 300 o 500"
+                        />
+                  </div>
+
+                  <input
+                        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+                        type="submit"
+                        value="Guardar Comida o Guardar Ejercicio"
+                  />
 
             </form>
       )

a1384513dcf62369794fd922c7b192e2aa9fc8cf
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-08-23 20:28:14 +0200

    storing the id to edit

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,13 +1,15 @@
 import { Activity } from "../types"
 import { categories } from "../data/categories";
-import { useMemo } from "react";
+import { useMemo, Dispatch } from "react";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityProps = {
-      activities: Activity[]
+      activities: Activity[],
+      dispatch: Dispatch<ActivityActions>
 }
 
-const ActivityList = ({ activities }: ActivityProps) => {
+const ActivityList = ({ activities, dispatch }: ActivityProps) => {
 
       const categoryName = useMemo(() =>
             (category: Activity['category']) => categories.map((cat => cat.id === category ? cat.name : ''))
@@ -19,22 +21,24 @@ const ActivityList = ({ activities }: ActivityProps) => {
                         Comida y Actividades
                   </h2>
 
-                  {activities.map(activitie => (
-                        <div key={activitie.id} className="px-5 py-10 bg-slate-100 mt-5 flex justify-between">
+                  {activities.map(activity => (
+                        <div key={activity.id} className="px-5 py-10 bg-slate-100 mt-5 flex justify-between">
                               <div className="space-y-2 relative">
                                     <p className={`absolute -top-8 -left-8 rounded-sm px-10 py-2 text-white uppercase font-bold
-                                           ${activitie.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
-                                          {categoryName(+activitie.category)}
+                                           ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                                          {categoryName(+activity.category)}
                                     </p>
-                                    <p className="text-2xl font-bold">{activitie.name}</p>
+                                    <p className="text-2xl font-bold">{activity.name}</p>
                                     <p className="font-black text-4xl text-lime-500">
-                                          {activitie.calories} {''}
+                                          {activity.calories} {''}
                                           <span>Calories</span>
                                     </p>
                               </div>
 
                               <div className="flex gap-5 items-center">
-                                    <button>
+                                    <button
+                                          onClick={() => dispatch({ type: "set-activeId", payload: { id: activity.id } })}
+                                    >
                                           <PencilSquareIcon className=" h-8 w-8 text-gray-800" />
                                     </button>
                               </div>
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -2,7 +2,7 @@ import { Activity } from "../types"
 
 export type ActivityActions =
       { type: 'save-activity', payload: { newActivity: Activity } } |
-      { type: 'set-active', payload: { id: Activity['id'] } }
+      { type: 'set-activeId', payload: { id: Activity['id'] } }
 
 type ActivityState = {
       activities: Activity[],
@@ -26,7 +26,7 @@ export const activityReducer = (
             }
       }
 
-      if (action.type === 'set-active') {
+      if (action.type === 'set-activeId') {
             return {
                   ...state,
                   activeId: action.payload.id

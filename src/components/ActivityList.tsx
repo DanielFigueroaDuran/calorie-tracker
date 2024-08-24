7bd7da66d4ef7ad14342f55ca0f6aa9726c7fb10
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-08-24 09:57:32 +0200

    Creating the action to restart the app

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -28,7 +28,7 @@ const ActivityList = ({ activities, dispatch }: ActivityProps) => {
                         ? <p className="text-center my-5">No hay actividades aún...</p>
 
                         : activities.map(activity => (
-                              <div key={activity.id} className="px-5 py-10 bg-slate-100 mt-5 flex justify-between">
+                              <div key={activity.id} className="px-5 py-10  mt-5 flex justify-between bg-slate-100 shadow-lg">
                                     <div className="space-y-2 relative">
                                           <p className={`absolute -top-8 -left-8 rounded-sm px-10 py-2 text-white uppercase font-bold
                                            ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
